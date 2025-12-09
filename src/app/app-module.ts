fe0b9c75696debf767cadcaf8758509b4725e7a2
Adelina Apetrei <adelina.apetrei26@gmail.com>
2025-12-09 22:05:13 +0200

    Add first component and service to BE

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,17 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { ProductList } from './components/product-list/product-list';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductService } from './services/product-service';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    ProductList
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    ProductService
   ],
   bootstrap: [App]
 })
